9b8613ff5090936b0f9d5896cfc167984b0fddb4
Abdi Adem <abdiadem954@gmail.com>
2021-10-24 21:02:34 +0300

    feat: done with rendering daily pictures

--- src/App.svelte
+++ src/App.svelte
@@ -1,8 +1,19 @@
 <script>
-	let app_name = ''
+		
+	const url = 'https://api.nasa.gov/planetary/apod?api_key=tJOWsZ9xvBHgXl4E58wve64bht5tkY0UZaO9Zgq0'
+
+	let content = null
+
+	let promise = fetch(url)
+			     .then(res => res.json())
+			     .then(data => {
+			     	content = data
+			     })
+			     .catch(err => { alert(err) })
+
 </script>
 
-<main>
+<div class="main">
 	<header>
 		<div class="brand">
 			<h1>AsPiOD</h1>
@@ -18,15 +29,50 @@
 	        </a>
 		</p>
 	</header>
-</main>
+
+	{#await promise}
+		loading ...
+	{:then data}
+
+		<div class="main-content">
+			<img src={ content.url } alt={ content.url }>
+
+			<div class="text-content">
+				<h2 class="title">{ content.title }</h2>
+				<p class="date">
+					<span class="bold">Date:</span> { content.date }
+				</p>
+				<p class="explanation">
+					{ content.explanation }
+				</p>
+				{#if content.copyright}
+					<p class="copyright">
+						&copy; content.copyright
+					</p>
+				{/if}
+				{#if content.hdurl}
+					<a class="download-btn" href={ content.hdurl } title={ content.hdurl }>View HD</a>
+				{/if}
+			</div>
+		</div>
+
+	{:catch error}
+
+	{/await}
+
+</div>
 
 <style>
-	main {
+	.bold {
+		font-weight: 800;
+	} 
+
+	.main {
 		width: 70%;
 		margin: 2.4em auto;
 	}
 
-	main header {
+	.main header {
 		color: #FEFEFE;
 		display: flex;
 		align-items: flex-end;
@@ -36,30 +82,68 @@
 		border-bottom: 0.1em solid #FEFEFE;
 	}
 
-	main header h1 {
+	.main header h1 {
 		font-size: 3.2em;
 		margin-bottom: 0.1em;
 	}
 
-	main header h3 {
+	.main header h3 {
 		margin-bottom: 0.4em;
 		font-size: 1.2em;	
+		font-weight: 300;
 	}
 
-	main header p {
+	.main header p {
 		display: flex;
 		align-items: center;
 		justify-content: center;
 	}
 
-	main header p * {
-		font-size: 0.9em;
+	.main header p * {
+		/*font-size: 0.9em;*/
 		font-family: 'Roboto';
 		font-weight: 200;
 	}
 
-	main header p span {
+	.main header p span {
 		margin-right: 1em;
+	}
+
+	.main-content {
+		margin-top: 2em;
+		display: flex;
+		justify-content: space-between;
+	}
+	
+	.main-content > img {
+		width: 45%;
+		border-radius: 0.4em;
+	}
+
+	.main-content .text-content {
+		margin-left: 1.2em;
+	}
+
+	.main-content .text-content h2 {
+		color: #FEFEFE;
+		font-size: 2em;
+		margin-bottom: 0.4em;
+	}
+
+	.main-content .text-content p {
+		color: #FEFEFE;
+		font-weight: 300;
+		margin-top: 0.6em;
+	}
+
+	.main-content .text-content .download-btn {
+		padding: 0.6em 2em;
+		background-color: var(--accent-color);
+		border-radius: 0.2em;
+		text-decoration: none;
+		margin-top: 1em;
+		display: inline-block;
+		color: #FEFEFE;
 	}
 
 
