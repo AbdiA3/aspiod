b60c0c29712acad9279db98ba437ebfd06514952
Abdi Adem <abdiadem954@gmail.com>
2021-10-25 08:00:31 +0300

    refactor: put the main content to its own component

--- src/App.svelte
+++ src/App.svelte
@@ -1,4 +1,5 @@
 <script>
+	import MainContent from './components/MainContent.svelte'
 		
 	const url = 'https://api.nasa.gov/planetary/apod?api_key=tJOWsZ9xvBHgXl4E58wve64bht5tkY0UZaO9Zgq0'
 
@@ -33,29 +34,7 @@
 	{#await promise}
 		loading ...
 	{:then data}
-
-		<div class="main-content">
-			<img src={ content.url } alt={ content.url }>
-
-			<div class="text-content">
-				<h2 class="title">{ content.title }</h2>
-				<p class="date">
-					<span class="bold">Date:</span> { content.date }
-				</p>
-				<p class="explanation">
-					{ content.explanation }
-				</p>
-				{#if content.copyright}
-					<p class="copyright">
-						&copy; { content.copyright }
-					</p>
-				{/if}
-				{#if content.hdurl}
-					<a class="download-btn" href={ content.hdurl } title={ content.hdurl }>View HD</a>
-				{/if}
-			</div>
-		</div>
-
+		<MainContent { content } />
 	{:catch error}
 		{ alert(error) }
 	{/await}
@@ -63,9 +42,6 @@
 </div>
 
 <style>
-	.bold {
-		font-weight: 800;
-	} 
 
 	.main {
 		width: 70%;
@@ -111,57 +87,11 @@
 		margin-right: 1em;
 	}
 
-	.main-content {
-		margin-top: 2em;
-		margin-bottom: 2em;
-		display: flex;
-		justify-content: space-between;
-	}
-	
-	.main-content > img {
-		height: 100%;
-		width: 40%;
-		border-radius: 0.4em;
-		object-fit: cover;
-		object-position: center;
-	}
-
-	.main-content .text-content {
-		margin-left: 1.2em;
-	}
-
-	.main-content .text-content h2 {
-		color: #FEFEFE;
-		font-size: 2em;
-		margin-bottom: 0.4em;
-	}
-
-	.main-content .text-content p {
-		color: #FEFEFE;
-		font-weight: 300;
-		margin-top: 0.6em;
-	}
-
-	.main-content .text-content .download-btn {
-		padding: 0.6em 2em;
-		background-color: var(--accent-color);
-		border-radius: 0.2em;
-		text-decoration: none;
-		margin-top: 1em;
-		display: inline-block;
-		color: #FEFEFE;
-	}
 
 	@media (max-width: 960px) {
 		.main {
 			/*display: none;*/
 			width: 80%;
-		}
-
-		.main-content {
-			margin-top: 2em;
-			display: flex;
-			justify-content: space-between;
 		}
 	}
 
@@ -177,24 +107,6 @@
 			justify-content: center;
 			text-align: center;
 		}
-
-		.main-content {
-			margin-top: 2em;
-			display: flex;
-			flex-direction: column;
-			justify-content: space-between;
-		}
-
-		.main-content .text-content {
-			margin-left: 0;
-			margin-top: 1em;
-			margin-bottom: 2em;
-		}
-
-		.main-content > img {
-			width: 100%;
-		}
 	}
-
 
 </style>
